7795fc56a63976c4bdb7f3f7258c681af0844c5f
Gavin Leake <gavinl@highlandspringgroup.com>
2012-04-30 01:34:46 +0100

    Found and fixed a buggy latlng, logged all the invalid postcodes, created a kml (for google maps) with many bugs removed.

--- chscotInvests.py
+++ chscotInvests.py
@@ -1,11 +1,11 @@
-# 914 rows in combined (inc header)
-
+import sys
 import csv
 import string
 import re
 import requests
 import json
 import time
+from xml.sax.saxutils import escape
 
 investsFile = open('investsCombined.csv','rU') #r = read, b = binary, U = universal (e.g. multilines)
 invests = csv.DictReader(investsFile, delimiter=',', dialect='excel')
@@ -56,12 +56,43 @@ assert validCount == len(valid)
 #print 'valid names: '+str(validCount)
 #print 'invalid names: '+str(invalidCount)
 # Celtic Colours Festival Society [B1P 5T9] appears to be in Canada
+#sys.exit(0)
 
 # do this once and put it in a csv (do not hammer google every time)
+#for row in valid:
+    #r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address='+row['postcode'])
+    #d = json.loads(r.text)
+    #lat = d['results'][0]['geometry']['location']['lat']
+    #lng = d['results'][0]['geometry']['location']['lng']
+    #print row['postcode']+','+str(lat)+','+str(lng)
+    #time.sleep(0.5)
+
+# Google API thinks PA45 5TT in Germany, should have used 'UK' in search, manual overwrite with 55.86894220,-6.04611130
+
+postcodesFile = open('latlngs.csv','r') #r = read, b = binary, U = universal (e.g. multilines)
+postcodesCSV = csv.reader(postcodesFile, delimiter=',')
+
+postcodes = {}
+
+for postcodeRow in postcodesCSV:
+    postcodes[postcodeRow[0]] = [postcodeRow[1],postcodeRow[2]]
+
+print '''<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://earth.google.com/kml/2.2">
+<Document>
+    <name>Culture Hack Creative Scotland Investments</name>
+    <description>Map of all the pinable (postcoded) investments made by Creative Scotland</description>
+    <open>1</open>
+'''
+
+print '<Folder><name>Investments</name>'
 for row in valid:
-    r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address='+row['postcode'])
-    d = json.loads(r.text)
-    lat = d['results'][0]['geometry']['location']['lat']
-    lng = d['results'][0]['geometry']['location']['lng']
-    print row['postcode']+','+str(lat)+','+str(lng)
-    time.sleep(0.5)
+    if row['project_title'].strip() != '':
+        name = row['applicant_name'].strip()+" - "+row['project_title'].strip()
+    else:
+        name = row['applicant_name'].strip()
+    descr = row['description'].strip()+" (Value of "+row['amount'].strip()+" awarded "+row['decision_date'].strip()+")"
+    print '<Placemark><name>'+escape(name)+'</name><description>'+escape(descr)+'</description><styleUrl>#BalloonStyle</styleUrl><Point><coordinates>'+postcodes[row['postcode']][1]+','+postcodes[row['postcode']][0]+',0</coordinates></Point></Placemark>'
+print '</Folder>'
+
+print '</Document></kml>'
